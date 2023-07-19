4ada2cc2d4381e1b2d636574060a636a7918cfa4
= <=>
2023-07-19 23:32:16 +0300

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,17 +1,14 @@
 import random
 
-# Generate a random number between -10,000 and 10,000
 number = random.randint(-10000, 10000)
 
-# Calculate the last digit of the number
 last_digit = abs(number) % 10
 if number < 0:
-    last_digit = -last_digit
-# Print the last digit and its properties.
-print("Last digit of", number, "is", last_digit, "and is", end=" ")
+    last_digit=-last_digit
+print("Last digit of", number, "is", last_digit, end=" ")
 if last_digit > 5:
-    print("greater than 5")
+    print("and is greater than 5")
 elif last_digit == 0:
-    print("0")
+    print("and is 0")
 else:
-    print("less than 6 and not 0")
+    print("and is less than 6 and not 0")
