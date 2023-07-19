51de0c49d0ebcad47a6db4428096ce8bb7686d87
= <=>
2023-07-19 20:41:44 +0300

    Another Method

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,18 @@
 import random
 
+# Generate a random number between -10,000 and 10,000
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+# Calculate the last digit of the number
+last_digit = number % 10
 
-print("Last digit of", number, "is", last_digit, end=" ")
+# Print the last digit and its properties
+print(f"Last digit of {number} is {last_digit} and is", end=" ")
 if last_digit > 5:
-    print("and is greater than 5")
+ print("greater than 5")
+elif last_digit < -5:
+ print("less than -5")
 elif last_digit == 0:
-    print("and is 0")
+ print("0")
 else:
-    print("and is less than 6 and not 0")
+ print("less than 6 and not 0")
