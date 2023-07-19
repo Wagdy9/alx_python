2c568ae6eafe848691e307b406f23e102c9ee491
= <=>
2023-07-19 20:46:53 +0300

    Another Method

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,12 +7,10 @@ number = random.randint(-10000, 10000)
 last_digit = number % 10
 
 # Print the last digit and its properties
-print(f"Last digit of {number} is {last_digit} and is", end=" ")
+print("Last digit of", number, "is", last_digit, "and is", end=" ")
 if last_digit > 5:
- print("greater than 5")
-elif last_digit < -5:
- print("less than -5")
+    print("greater than 5")
 elif last_digit == 0:
- print("0")
+    print("0")
 else:
- print("less than 6 and not 0")
+    print("less than 6 and not 0")
