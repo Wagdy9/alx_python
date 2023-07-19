f2be8924936e90f962e8ea75d7c7f16784b09f39
= <=>
2023-07-19 23:53:07 +0300

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,6 @@
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d}".format(tens_digit * 10 + ones_digit), end="")
-        if ones_digit != 8:
-            print(", ", end="")
-        else:
-            print()
+        print(
+            "{:d}{:d}".format(tens_digit, ones_digit),
+            end=", " if tens_digit < 8 or ones_digit < 9 else "\n",
+        )
