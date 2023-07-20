ddf1a758c54845b16f74c1161704d995ea685c7f
= <=>
2023-07-20 17:03:08 +0300

    a ^ b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+pow = __import__("1-power").pow
+
+
 def pow(a, b):
     result = 1
     if b >= 0:
@@ -8,12 +11,3 @@ def pow(a, b):
         for _ in range(-b):
             result /= a
     return result
-
-
-pow = __import__("1-power").pow
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
