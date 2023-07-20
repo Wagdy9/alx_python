93c0388ebbde75dfc868c15f3f40085ab5aea959
= <=>
2023-07-20 17:50:46 +0300

    a ^ b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def pow(a, b):
-    result = 1
-    if b >= 0:
-        for _ in range(b):
-            result *= a
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
+    elif b % 2 == 0:
+        return pow(a * a, b // 2)
     else:
-        for _ in range(-b):
-            result /= a
-    return result
+        return a * pow(a, b - 1)
