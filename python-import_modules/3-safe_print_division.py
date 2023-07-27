e6ae4ab15fd0ffcad43163d0de8c75cee1799d15
= <=>
2023-07-27 17:32:20 +0300

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,34 +3,6 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-    except TypeError:
-        result = None
     finally:
         print("Inside result: {}".format(result))
-
-
-# Example usage
-a = 10
-b = 2
-safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, a / b))
-
-a = 10
-b = -2
-safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, a / b))
-
-a = 0
-b = 2
-safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, a / b))
-
-a = 10
-b = 0
-safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, a / b))
-
-a = 0
-b = 0
-safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, a / b))
+        return result
