4ad8cb2fa16212ba0d68de91c470cc698525b3ce
= <=>
2023-07-27 15:54:49 +0300

    Integers division with debug

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,26 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Inside result: None")
+        return None
+    except TypeError:
+        print("Inside result: None")
+        return None
+    else:
+        print("Inside result: {}".format(result))
+        return result
+    finally:
+        print("Finally: Division operation completed")
+
+
+# Example usage
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
