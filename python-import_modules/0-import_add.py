3def8e3982f18a7e910b4b5a4b80006011bfde3e
= <=>
2023-07-27 18:35:02 +0300

    Import a simple function from a simple file

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,32 +1,11 @@
+#!/usr/bin/python3
+
+a = 1
+b = 2
+
 if __name__ == "__main__":
     from add_0 import add
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
 
-    a = 1
-    b = 2
-
-    result = add(a, b)
-    print(f"Correct output - case: a = {a} and b = {b} FAKE add() => {a} - {b}")
-
-    a = 10
-    b = 30
-
-    result = add(a, b)
-    print(f"Correct output - case: a = {a} and b = {b} FAKE add() => {a} - {b}")
-
-    a = -10
-    b = 30
-
-    result = add(a, b)
-    print(f"Correct output - case: a = {a} and b = {b} FAKE add() => {a} - {b}")
-
-    a = -10
-    b = -30
-
-    result = add(a, b)
-    print(f"Correct output - case: a = {a} and b = {b} FAKE add() => {a} - {b}")
-
-    a = 5
-    b = "H"
-
-    result = add(a, b)
-    print(f"Correct output - case: a = {a} and b = {b} FAKE add() => {a} - {b}")
+```
