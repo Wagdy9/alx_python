05d6da90ebc17f3608e9521e1fe7d08645272edf
= <=>
2023-07-27 15:46:54 +0300

    Everything can be imported

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,15 +1,3 @@
-import importlib.util
+from variable_load_2 import a
 
-def print_variable_value(file_path, variable_name):
-    spec = importlib.util.spec_from_file_location("module_name", file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    if hasattr(module, variable_name):
-        variable_value = getattr(module, variable_name)
-        print(variable_value)
-
-if __name__ == "__main__":
-    file_path = "variable_load_2.py"
-    variable_name = "a"
-    print_variable_value(file_path, variable_name)
+print(a)
