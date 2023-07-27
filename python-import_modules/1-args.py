b7a04401eebd8ee71b94467f17d26231dc7355c6
= <=>
2023-07-27 17:41:57 +0300

    How to make a script dynamic!

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,17 +1,22 @@
 import sys
 
-def print_arguments(argv):
-    num_args = len(argv)
-    if num_args == 0:
-        print("0 arguments.")
-        print(".")
-    elif num_args == 1:
-        print("1 argument:")
-        print("1:", argv[0])
-    else:
-        print(num_args, "arguments:")
-        for i, arg in enumerate(argv, 1):
-            print(i, ":", arg)
 
-if __name__ == "__main__":
-    print_arguments(sys.argv[1:])
+def print_arguments():
+    arguments = sys.argv[1:]  # Exclude the script name from the arguments list
+    num_arguments = len(arguments)
+
+    # Print the number of arguments
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        if num_arguments == 1:
+            print("1 argument:")
+        else:
+            print(f"{num_arguments} arguments:")
+
+        # Print each argument with its position
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
+
+
+print_arguments()
