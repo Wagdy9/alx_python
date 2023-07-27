e980ca0b67f703cad26efb38b41df0618a21dbd4
= <=>
2023-07-27 15:59:26 +0300

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -7,31 +7,30 @@ def safe_print_division(a, b):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        return result
 
 
 # Example usage
 a = 10
 b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, a / b))
 
 a = 10
 b = -2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, a / b))
 
 a = 0
 b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, a / b))
 
 a = 10
 b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, a / b))
 
 a = 0
 b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, a / b))
