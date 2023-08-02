03a713ed2f22051689be53c8b4e5392dec163101
wagdy9 <wagdym@gmailcom>
2023-08-02 22:51:53 +0300

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,6 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
@@ -19,8 +18,3 @@ except AttributeError as e:
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
-
-try:
-    print(my_square._Square__size)
-except Exception as e:
-    print(e)
