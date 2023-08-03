4d9860bbc4e3849d777f7b19bf3744a4577ceee6
= <=>
2023-08-03 15:02:23 +0300

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,24 +1,12 @@
 class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
+    def __init__(self size=0):
+        if isinstance(size int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
+    
     def area(self):
-        return self.__size**2
-
-
-mysquare = Square(3)
-print("{}".format(mysquare.area()))
-
-mysquare = Square(89)
-print("{}".format(mysquare.area()))
-
-mysquare = Square()
-print("{}".format(mysquare.area()))
+        return self.__size ** 2
