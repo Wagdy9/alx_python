20f19d0b14ac8047c6b33c6e45a60f494ab12fa2
= <=>
2023-08-03 14:39:18 +0300

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,26 @@
 class Square:
+    """
+    This class represents a Square.
+
+    Attributes:
+        __size (int): The size of the Square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the Square.
+        """
         self.__size = size
+
+    @property
+    def dict_(self):
+        """
+        Retrieves the Square attributes as a dictionary.
+
+        Returns:
+            dict: A dictionary containing the Square attributes.
+        """
+        return self.__dict__
