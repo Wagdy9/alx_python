ad6bce2bbab3aea282e158eb93ad15ad85be9438
= <=>
2023-08-03 23:03:23 +0300

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,25 @@
 class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
