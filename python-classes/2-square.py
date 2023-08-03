18e9a1066c6926e4467c93bfe1e13a6f02e11f60
= <=>
2023-08-03 14:58:25 +0300

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,18 +14,11 @@ class Square:
         return self.__size**2
 
 
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
+mysquare = Square(3)
+print("{}".format(mysquare.area()))
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+mysquare = Square(89)
+print("{}".format(mysquare.area()))
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+mysquare = Square()
+print("{}".format(mysquare.area()))
