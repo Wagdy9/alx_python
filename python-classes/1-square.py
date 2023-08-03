ec867cca63521f22bf0bcdb7cdb24b97a14598c6
= <=>
2023-08-03 22:43:28 +0300

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,24 +1,3 @@
-class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-
-class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def __str__(self):
-        return f"Square: {self.__size}"
-
-
 my_square_1 = Square(3)
 print(type(my_square_1))
 print(my_square_1.__dict__)
