02fe236ba6d9e257508cc27f0c658b1b0e2309ed
= <=>
2023-08-03 23:30:34 +0300

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,28 +1,37 @@
+"""
+This module defines the Square class, which represents a square shape.
+
+Classes:
+    Square: A class that defines a square shape.
+"""
+
+
 class Square:
+    """
+    A class that defines a square shape.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+    """
     def __init__(self, size=0):
+                """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         return self.__size**2
-
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
