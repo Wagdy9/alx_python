36f756fe727cd48b8ec602c706110d873fad9e64
= <=>
2023-08-03 15:06:29 +0300

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,12 @@
 class Square:
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            raise TypeError("size must be an integer")
 
     def area(self):
-        return self.__size ** 2
+        return self.__size**2
