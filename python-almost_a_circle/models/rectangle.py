47426ce47878f32b9ef2dcde47ce83bcd98fae9e
= <=>
2023-08-09 20:10:38 +0300

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,6 +73,137 @@ class Rectangle(Base):
             raise ValueError("y must be positive")
         self.__y = value
 
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class
+
+    A rectangle with width, height, x and y attributes.
+    """
+
+    __width = None
+    __height = None
+    __x = None
+    __y = None
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Init method
+
+        Args:
+            width: The width of the rectangle.
+            height: The height of the rectangle.
+            x: The x-coordinate of the rectangle.
+            y: The y-coordinate of the rectangle.
+            id: The id of the rectangle.
+
+        Raises:
+            TypeError: If width is not an integer.
+            TypeError: If height is not an integer.
+            TypeError: If x is not an integer.
+            TypeError: If y is not an integer.
+            ValueError: If width is less than 0.
+            ValueError: If height is less than 0.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter
+
+        Args:
+            value: The new width of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be positive")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter
+
+        Args:
+            value: The new height of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be positive")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter
+
+        Args:
+            value: The new x-coordinate of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be positive")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter
+
+        Args:
+            value: The new y-coordinate of the rectangle.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be positive")
+        self.__y = value
+
+    def __repr__(self):
+        """Representation"""
+        return f"<Rectangle id={self.id} width={self.width} height={self.height} x={self.x} y={self.y}>"
+
     def __repr__(self):
         """Representation"""
         return f"<Rectangle id={self.id} width={self.width} height={self.height} x={self.x} y={self.y}>"
