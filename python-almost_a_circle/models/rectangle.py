3f588cd89d12eba7aa3924bce7056cfbbaf951ff
= <=>
2023-08-09 20:08:44 +0300

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class"""
+
+    __width = None
+    __height = None
+    __x = None
+    __y = None
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Init method"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be positive")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be positive")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be positive")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be positive")
+        self.__y = value
+
+    def __repr__(self):
+        """Representation"""
+        return f"<Rectangle id={self.id} width={self.width} height={self.height} x={self.x} y={self.y}>"
