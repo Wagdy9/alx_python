359b4de0ffdc046c393c804c8a3053feffb4ec84
= <=>
2023-08-09 20:47:20 +0300

    Area first

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,7 +83,3 @@ This class represents a rectangle.
 
 * **__validate_width** (self, width):
     Validator for the width attribute.
-    * **Arguments:**
-        * `width`: The width of the rectangle.
-    * **Raises** `TypeError` if `width
-
