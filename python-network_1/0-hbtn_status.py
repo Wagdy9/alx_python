6385472a79bf6c229e63e8786af4e512646b8dc0
= <=>
2023-08-10 23:04:27 +0300

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,8 @@
-"""This module fetches the status of the HTB intranet and prints the body of the response."""
-
 import requests
 
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
 
-def get_status():
-    response = requests.get("https://intranet.hbtn.io/status")
-    content = response.content.decode("utf-8")
-    return content
-
-
-if __name__ == "__main__":
-    print("Body response:")
-    print("\t- type: {_type}".format(_type=type(content)))
-    print("\t- content: {_content}".format(_content=content))
-    print("\t- utf8 content: {_utf8_c}".format(_utf8_c=content))
-
-print(get_status())
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
