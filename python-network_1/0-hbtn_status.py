bbf40ccef5eb9ed3b95319ced56a4f790a3d52ef
wagdy9 <wagdym@gmailcom>
2023-08-10 03:51:08 +0300

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,39 @@
-def get_body(self):
-    """Gets the body of the response and decodes it as JSON."""
-    if self.response.status_code == 200:
-        return json.loads(self.response.content.decode())
+"""This module fetches the status of the HTB intranet and prints the body of the response."""
+
+import requests
+import json
+
+
+class HTBStatus:
+    """Gets the status of the HTB intranet."""
+
+    def __init__(self):
+        """Initializes the HTBStatus object."""
+        self.response = requests.get("https://intranet.hbtn.io/status")
+
+    def get_body(self):
+        """Gets the body of the response and decodes it as JSON.
+
+        Returns:
+          A dictionary containing the response data.
+        """
+        if self.response.status_code == 200:
+            return json.loads(self.response.content.decode())
+        else:
+            return None
+
+
+def main():
+    """Prints the body of the response."""
+    hbtn_status = HTBStatus()
+    body = hbtn_status.get_body()
+    if body:
+        print("Body response:\n")
+        for key, value in body.items():
+            print(f"  - {key}: {value}")
     else:
-        return None
+        print("No status information available.")
+
+
+if __name__ == "__main__":
+    main()
