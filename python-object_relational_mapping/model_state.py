ebae4660bb001466047182402e8a4f84307e29e5
= <=>
2023-08-24 19:07:18 +0300

    Full

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,26 +1,21 @@
-"""
-Represents a State table in the database.
-"""
-
-
-import sys
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# Create a database engine using SQLAlchemy's create_engine function
-# It connects to a MySQL database using the provided username, password, and database name
-
-# Create a base class for declarative models
 Base = declarative_base()
+
 
 class State(Base):
     """
-    Represents a State table in the database.
-    
-    It defines columns for 'id' and 'name'.
+     Maps to the 'states' table.
+
+    Attributes:
+        id (int): The unique identifier for the state.
+        name (str): The name of the state.
+
+    Args:
+        Base (DeclarativeMeta): The base class.
     """
-    __tablename__ = 'states'
+
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
