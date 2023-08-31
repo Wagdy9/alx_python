b677f5e2f9bf0716696ae3e8bda016d80b6d0190
= <=>
2023-08-31 12:05:58 +0300

    Full

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -51,6 +51,7 @@ def python_text(text):
     """
     return "Python " + text.replace("_", " ")
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """
@@ -68,18 +69,26 @@ def number_template(n):
 
 
 @app.route("/number_odd_or_even/<n>")
-def number_odd_or_even(n):
+def odd_or_even(n):
     """
-    Route handler for the root URL "/number_odd_or_even/<n>". Displays a string.
+    A route handler for the URL '/number_odd_or_even/<n>'.
+    It returns an HTML page with an H1 tag containing "Number: is even|odd".
+
+    Args:
+        n (int): The value of the number
+
+    Returns:
+        str: HTML page with H1 tag.
     """
     try:
         n = int(n)
         if n % 2 == 0:
-            return render_template("6-number_odd_or_even.html", number=n, number_type="even")
+            return render_template("6-number_odd_or_even.html", n=n, status="even")
         else:
-            return render_template("6-number_odd_or_even.html", number=n, number_type="odd")
+            return render_template("6-number_odd_or_even.html", n=n, status="odd")
     except ValueError:
         return "404 Not Found", 404
+
 
 if __name__ == "__main__":
     """
